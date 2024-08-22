4a41481e9b401af161d2ec3a35eae391f8ba6add
saijami <saijami@unosimple.com>
2024-08-22 19:13:47 +0530

    getAllUsers

--- server/resolvers.js
+++ server/resolvers.js
@@ -12,5 +12,15 @@ export const resolvers = {
         throw new Error("Failed to fetch todos");
       }
     },
+    getAllUsers: async () => {
+      try {
+        const result = await axios.get(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        return result.data;
+      } catch (error) {
+        throw new Error("Failed to fetch users");
+      }
+    },
   },
 };
--- server/typeDefs.js
+++ server/typeDefs.js
@@ -6,7 +6,17 @@ type Todo{
     completed: Boolean
 }
 
+type User {
+    id: ID!    
+    name: String!
+    username: String!
+    email: String!
+    phone: String!
+    website: String!
+}
+
 type Query{
     getTodos: [Todo]
+    getAllUsers: [User]
 }
 `;
